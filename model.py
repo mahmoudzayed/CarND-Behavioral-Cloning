773ebf147b2eb219f7f93cde752c9022862794f4
MahmoudZayed <mahmoud.moataz.zayed@gmail.com>
2018-07-20 15:52:34 +0200

    clean code

--- model.py
+++ model.py
@@ -2,10 +2,6 @@ import csv
 import cv2
 import numpy as np
 import tensorflow as tf
-import sklearn
-import os
-from random import shuffle
-from sklearn.model_selection import train_test_split
 
 samples = []
 
@@ -17,46 +13,8 @@ with open('data/driving_log.csv') as csvfile:
 		samples.append(line)
 		
 
-train_samples, validation_samples = train_test_split(samples, test_size=0.2)
-
 images = []
 angles = []
-"""
-ch, row, col = 3, 80, 320  # Trimmed image format
-
-def generator(samples, batch_size=128):
-    num_samples = len(samples)
-    while 1: # Loop forever so the generator never terminates
-        shuffle(samples)
-        for offset in range(0, num_samples, batch_size):
-            batch_samples = samples[offset:offset+batch_size]
-            images = []
-            angles = []
-            for batch_sample in batch_samples:
-                for i in range(3):
-                    name = 'data/IMG/'+batch_sample[i].split('/')[-1]
-                    image = cv2.imread(name)
-                    measurment = float(batch_sample[3])
-                    images.append(image)
-                    if i == 0 :
-                        angles.append(measurment)
-                        image_flipped = cv2.flip(cv2.imread(name),1)
-                        images.append(image_flipped)
-                        angles.append(measurment*-1.0)
-                    if i == 1 :
-                        angles.append(measurment+0.2)
-                    if i == 2 :
-                        angles.append(measurment-0.2)
-
-            # trim image to only see section with road
-            X_train = np.array(images)
-            y_train = np.array(angles)
-            yield sklearn.utils.shuffle(X_train, y_train)
-	
-# compile and train the model using the generator function
-train_generator = generator(train_samples, batch_size=1000)
-validation_generator = generator(validation_samples, batch_size=1000)
-"""
 	
 for batch_sample in samples:
     for i in range(3):
